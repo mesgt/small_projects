9494f9de772cfcc1acbb9d9af0ac03966bbf8bb0
Maria Sargent <75868918+mesgt@users.noreply.github.com>
2021-05-05 23:00:22 -0500

    fully functional prev and next buttons with responsive progress bar.

--- script.js
+++ script.js
@@ -29,6 +29,15 @@ function update() {
     const actives = document.querySelectorAll('.active1')
 
     progress.style.width = ((actives.length-1) / (circles.length-1)) * 100 + '%'
+
+    if(currentActive1 === 1) {
+        prev.disabled = true
+    } else if(currentActive1 === circles.length) {
+        next.disabled = true
+    } else {
+        prev.disabled = false
+        next.disabled = false
+    }
 }
 
 let currentActive1 = 1
