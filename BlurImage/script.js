81e29bdee90e3629805d5822e4e5b640a320948b
Maria Sargent <75868918+mesgt@users.noreply.github.com>
2021-05-07 20:58:46 -0500

    Added scale funtion for blur effect.

--- BlurImage/script.js
+++ BlurImage/script.js
@@ -7,6 +7,14 @@ let int = setInterval(blurring, 30)
 
 function blurring() {
     load++
-
-    console.log(load)
+    if(load > 99) {
+        clearInterval(int)
+    }
+    
+    loadingText.innerText = `${load}%`
+    //map 0 to 100 with 100 to 0 for the opacity index//
+    loadingText.style.opacity = scale(load, 0, 100, 1, 0)
+    bg.style.filter = `blur(${scale(load, 0, 100, 20, 0)}px)`
 }
+
+const scale = (num, in_min, in_max, out_min, out_max) => {return (num-in_min) * (out_max - out_min) / (in_max - in_min) + out_min}
