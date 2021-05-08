f59f17699edc31e1c092719c46d4b0fb4dfe3fcf
Maria Sargent <75868918+mesgt@users.noreply.github.com>
2021-05-07 20:45:24 -0500

    Added blurring function to js.

---
+++ BlurImage/script.js
@@ -0,0 +1,11 @@
+const loadingText = document.querySelector('.loading-text');
+const bg = document.querySelector('.bg');
+
+let load = 0
+
+
+function blurring() {
+    load++
+
+    console.log(load)
+}
