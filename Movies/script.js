3c131f988c70b6b51d11ec84f496ceae70cfcf50
Maria Sargent <75868918+mesgt@users.noreply.github.com>
2021-05-25 22:35:32 -0500

    fetching most popular movies.

--- Movies/script.js
+++ Movies/script.js
@@ -1,2 +1,13 @@
 const API_URL = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=0b6c39d783c259a59051436ef6b9eecf&page=1'
+const IMG_PATH = 'https://image.tmdb.org/t/p/w1280'
+const SEARCH_URL = 'https://api.themoviedb.org/3/search/movie?api_key=0b6c39d783c259a59051436ef6b9eecf&query="'
 
+getMovies(API_URL)
+
+//get initial movies
+async function getMovies(url) {
+    const res = await fetch(url)
+    const data = await res.json()
+    
+    console.log(data.results)
+}
