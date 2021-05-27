0d533083f837705a5c5f790dd304e666efc6e247
Maria Sargent <75868918+mesgt@users.noreply.github.com>
2021-05-26 23:31:52 -0500

    digital display day of the week, month and date.

--- ThemeClock/script.js
+++ ThemeClock/script.js
@@ -45,10 +45,12 @@ function setTime() {
     const time = new Date();
     const month = time.getMonth()
     const day = time.getDay()
+    const date = time.getDate()
     const hours = time.getHours()
     const hoursForClock = hours % 12
     const minutes = time.getMinutes()
     const seconds = time.getSeconds()
+    const ampm = hours >=12 ? 'PM' : 'AM'
 
     console.log(time)
     
@@ -62,7 +64,9 @@ function setTime() {
 
     secondEl.style.transform = `translate(-50%, -100%) rotate(${scale(seconds, 0, 60, 0, 360)}deg)`
 
-    timeEl.innerHTML = `${hoursForClock}:${minutes < 10 ? `0${minutes}`: minutes}`
+    timeEl.innerHTML = `${hoursForClock}:${minutes < 10 ? `0${minutes}`: minutes} ${ampm}`
+
+    dateEl.innerHTML = `${days[day]}, ${months[month]} <span class="circle">${date}</span>`
 }
 
 setTime()
