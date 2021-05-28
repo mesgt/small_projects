dca66a7d8ab6dfecf43c2495d26450f7bc6497e6
Maria Sargent <75868918+mesgt@users.noreply.github.com>
2021-05-27 22:45:35 -0500

    hover over effect, image move to new div.

--- DragNDrop/script.js
+++ DragNDrop/script.js
@@ -12,25 +12,28 @@ for(const empty of empties) {
 }
 
 function dragStart() {
-    console.log('drag start')
+    this.className += ' hold'
+    setTimeout(() => this.className = 'invisible', 10)
 }
 
 function dragEnd() {
-    console.log('drag end')
+    this.className += ' fill'
 }
 
-function dragOver() {
-    console.log('drag over')
+function dragOver(e) {
+    e.preventDefault()
 }
 
-function dragEnter() {
-    console.log('drag enter')
+function dragEnter(e) {
+    e.preventDefault()
+    this.className += ' hovered'
 }
 
 function dragLeave() {
-    console.log('drag leave')
+    this.className = 'empty'
 }
 
 function dragDrop() {
-    console.log('drag drop')
+    this.className = 'empty'
+    this.append(fill)
 }
