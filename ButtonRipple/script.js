38615664257f396c405c9974cfd2842ee295192f
Maria Sargent <75868918+mesgt@users.noreply.github.com>
2021-05-27 21:29:25 -0500

    ripple effect added through javascript and timeout set.

--- ButtonRipple/script.js
+++ ButtonRipple/script.js
@@ -1,10 +1,24 @@
 const buttons = document.querySelectorAll('.ripple')
 
 buttons.forEach(button => {
-    button.addEventListener('click', (e) => {
+    button.addEventListener('click', function(e) {
+        //identify x and y coordinates on page
         const x = e.clientX
         const y = e.clientY
+        //offset for button
+        const buttonTop = e.target.offsetTop
+        const buttonLeft = e.target.offsetLeft
+        //target specific position within button
+        const xInside = x - buttonLeft
+        const yInside = y - buttonTop
 
-        console.log(x, y)
+        const circle = document.createElement('span')
+        circle.classList.add('circle')
+        circle.style.top = yInside + 'px'
+        circle.style.left = xInside + 'px'
+
+        this.appendChild(circle) //arrow function won't work with 'this'
+        //remove excess circles after clicking. 
+        setTimeout(() => circle.remove(), 500)
     })
 })
