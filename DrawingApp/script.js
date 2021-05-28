aa63957940478d242cbfd90d471ff4abf24d95dd
Maria Sargent <75868918+mesgt@users.noreply.github.com>
2021-05-27 23:10:10 -0500

    add filled or outline circle.

---
+++ DrawingApp/script.js
@@ -0,0 +1,18 @@
+const canvas = document.getElementById('canvas');
+const ctx = canvas.getContext('2d');
+
+let size = 20
+let color = 'black'
+
+
+function drawCircle(x, y) {
+    ctx.beginPath();
+    ctx.arc(x, y, size, 0, Math.PI * 2)
+    ctx.fillStyle = color
+    ctx.fill()
+    // ctx.stroke()
+}
+
+
+
+drawCircle(100, 200)
