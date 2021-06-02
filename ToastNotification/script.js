8f391830ae99da45c9acf9696495a26dbed18d0c
Maria Sargent <75868918+mesgt@users.noreply.github.com>
2021-06-02 12:22:58 -0500

    notifications display.

--- ToastNotification/script.js
+++ ToastNotification/script.js
@@ -14,5 +14,18 @@ const messages = [
 button.addEventListener('click', () => createNotification())
 
 function createNotification() {
-    console.log(123)
+    const notif = document.createElement('div')
+    notif.classList.add('toast')
+
+    notif.innerText = getRandomMessage()
+
+    toasts.appendChild(notif)
+
+    setTimeout(() => {
+        notif.remove()
+    }, 3000)
+}
+
+function getRandomMessage() {
+    return messages[Math.floor(Math.random() * messages.length)]
 }
