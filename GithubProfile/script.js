a3c5d5a0b03bf183fbfad8fdf6409a97b1b929e9
Maria Sargent <75868918+mesgt@users.noreply.github.com>
2021-06-07 16:55:41 -0500

    user info displayed.

--- GithubProfile/script.js
+++ GithubProfile/script.js
@@ -1,17 +1,40 @@
 const APIURL = "https://api.github.com/users/"
+const main = document.getElementById('main')
 const form = document.getElementById('form')
 const search = document.getElementById('search')
 
 
 async function getUser(username) {
     try {
-        const { data} = await axios(APIURL + username)
-
-        console.log(data)
+        const { data } = await axios(APIURL + username)
+        createUserCard(data)
     } catch(err) {
         console.log(err)
     }
 }
+
+function createUserCard(user) {
+    const cardHTML = `
+    <div class="card">
+    <div>
+        <img src="${user.avatar_url}" alt="${user.name}" class="avatar">
+    </div>
+    <div class="user-info">
+        <h2>${user.name}</h2>
+        <p>${user.bio}</p>
+        <ul>
+            <li>${user.followers} <strong>Followers</strong></li>
+            <li>${user.following} <strong>Following</strong></li>
+            <li>${user.public_repos} <strong>Repos</strong></li>
+        </ul>
+        <div class="repos"></div>
+    </div>
+</div>
+`
+
+    main.innerHTML = cardHTML
+}
+
 
 form.addEventListener('submit', (e) => {
     e.preventDefault()
