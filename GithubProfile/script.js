efce7c0e9bec3cf31261bd8a1d6cc16e5429ac47
Maria Sargent <75868918+mesgt@users.noreply.github.com>
2021-06-07 16:32:14 -0500

    event listener.

--- GithubProfile/script.js
+++ GithubProfile/script.js
@@ -1,7 +1,7 @@
 const APIURL = "https://api.github.com/users/"
+const form = document.getElementById('form')
+const search = document.getElementById('search')
 
-
-getUser('mesgt')
 
 async function getUser(username) {
     try {
@@ -12,3 +12,15 @@ async function getUser(username) {
         console.log(err)
     }
 }
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    const user = search.value
+
+    if(user) {
+        getUser(user)
+
+        search.value = ''
+    }
+})
