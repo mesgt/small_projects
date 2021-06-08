eb7dc6deb0f99d1ad982f384466d95a6d7de0cc9
Maria Sargent <75868918+mesgt@users.noreply.github.com>
2021-06-07 23:47:03 -0500

    repos search.

--- GithubProfile/script.js
+++ GithubProfile/script.js
@@ -8,10 +8,20 @@ async function getUser(username) {
     try {
         const { data } = await axios(APIURL + username)
         createUserCard(data)
+        getRepos(username)
     } catch(err) {
         if(err.response.status == 404) {
             createErrorCard('No profile with this user name')
         }
+    }
+}
+
+async function getRepos(username) {
+    try {
+        const { data } = await axios(APIURL + username + '/repos')
+        addReposToCard(data)
+    } catch(err) {
+        createErrorCard('There is a problem with fetching the repos')
     }
 }
 
@@ -24,28 +34,43 @@ function createErrorCard(msg) {
     main.innerHTML = cardHTML
 }
 
+
 function createUserCard(user) {
     const cardHTML = `
     <div class="card">
     <div>
-        <img src="${user.avatar_url}" alt="${user.name}" class="avatar">
+    <img src="${user.avatar_url}" alt="${user.name}" class="avatar">
     </div>
     <div class="user-info">
-        <h2>${user.name}</h2>
-        <p>${user.bio}</p>
-        <ul>
-            <li>${user.followers} <strong>Followers</strong></li>
-            <li>${user.following} <strong>Following</strong></li>
-            <li>${user.public_repos} <strong>Repos</strong></li>
-        </ul>
-        <div class="repos"></div>
+    <h2>${user.name}</h2>
+    <p>${user.bio}</p>
+    <ul>
+    <li>${user.followers} <strong>Followers</strong></li>
+    <li>${user.following} <strong>Following</strong></li>
+    <li>${user.public_repos} <strong>Repos</strong></li>
+    </ul>
+    <div class="repos"></div>
     </div>
-</div>
-`
-
+    </div>
+    `
+    
     main.innerHTML = cardHTML
 }
 
+function addReposToCard(repos) {
+    const reposEl = document.getElementById('repos')
+
+    repos
+    .forEach(repo => {
+        const repoEl = document.createElement('a')
+        repoEl.classList.add('repo')
+        repoEl.href = repo.html_url
+        repoEl.target = '_blank'
+        repoEl.innerText = repo.name 
+
+        reposEl.appendChild(repoEl)
+    })
+}
 
 form.addEventListener('submit', (e) => {
     e.preventDefault()
