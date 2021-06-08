7e57c12c8f1e8edcd52b09aaf60a3b08674439b3
Maria Sargent <75868918+mesgt@users.noreply.github.com>
2021-06-08 18:54:40 -0500

    event listener added.

--- PasswordGenerator/script.js
+++ PasswordGenerator/script.js
@@ -1,9 +1,28 @@
+const resultEl = document.getElementById('result')
+const lengthEl = document.getElementById('length')
+const uppercaseEl = document.getElementById('uppercase')
+const lowercaseEl = document.getElementById('lowercase')
+const numberEl = document.getElementById('numbers')
+const symbolEl = document.getElementById('symbols')
+const generateEl = document.getElementById('generate')
+const clipboardEl = document.getElementById('clipboard')
+
 const randomStuff = {
     lower: getRandomLower,
     upper: getRandomUpper,
     number: getRandomNumber,
     symbol: getRandomSymbol
 }
+
+generateEl.addEventListener('click', () => {
+    const length = +lengthEl.value
+    const hasLower = lowercaseEl.checked
+    const hasUpper = uppercaseEl.checked
+    const hasNumber = numberEl.checked
+    const hasSymbol = symbolEl.checked
+    
+    console.log(length, hasLower, hasUpper, hasNumber, hasSymbol)
+})
 
 function getRandomLower() {
     return String.fromCharCode(Math.floor(Math.random() * 26) + 97)
