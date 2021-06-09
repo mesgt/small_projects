31a36e11a14582da80f57bf5b22f042d5c3ac344
Maria Sargent <75868918+mesgt@users.noreply.github.com>
2021-06-08 19:14:55 -0500

    random password generated and displayed on page.

--- PasswordGenerator/script.js
+++ PasswordGenerator/script.js
@@ -21,8 +21,29 @@ generateEl.addEventListener('click', () => {
     const hasNumber = numberEl.checked
     const hasSymbol = symbolEl.checked
     
-    console.log(length, hasLower, hasUpper, hasNumber, hasSymbol)
+    resultEl.innerText = generatePassword(hasLower, hasUpper, hasNumber, hasSymbol, length)
 })
+
+function generatePassword(lower, upper, number, symbol, length) {
+    let generatedPassword = ''
+    const typesCount = lower + upper + number + symbol
+
+    const typesArr = [{lower}, {upper}, {number}, {symbol}].filter(item => Object.values(item)[0])
+    
+    if(typesCount === 0) {
+        return ''
+    }
+
+    for(let i = 0; i < length; i += typesCount) {
+        typesArr.forEach(type => {
+            const funcName = Object.keys(type)[0]
+            generatedPassword += randomStuff[funcName]()
+        })
+    }
+    const finalPassword = generatedPassword.slice(0, length)
+
+    return finalPassword
+}
 
 function getRandomLower() {
     return String.fromCharCode(Math.floor(Math.random() * 26) + 97)
@@ -40,6 +61,3 @@ function getRandomSymbol() {
     const symbols = '~!@#$%^&*()_+-=;:<>?,./"[]{}\|`'
     return symbols[Math.floor(Math.random() * symbols.length)]
 }
-
-console.log(getRandomLower())
-console.log(getRandomUpper(), getRandomNumber(), getRandomSymbol())
