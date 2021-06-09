58888de9709e99bb730dc699753485754931eda7
Maria Sargent <75868918+mesgt@users.noreply.github.com>
2021-06-09 18:57:50 -0500

    logic for slider exception added.

--- CheckBoxes/script.js
+++ CheckBoxes/script.js
@@ -1,1 +1,22 @@
+const toggles = document.querySelectorAll('.toggle')
 const cheap = document.getElementById('cheap')
+const good = document.getElementById('good')
+const fast = document.getElementById('fast')
+
+toggles.forEach(toggle => toggle.addEventListener('change', (e) => doTheTrick(e.target)))
+
+function doTheTrick(clickedOne) {
+    if(good.checked && cheap.checked && fast.checked) {
+        if(good === clickedOne) {
+            cheap.checked = false
+        }
+
+        if(cheap === clickedOne) {
+            good.checked = false
+        }
+        
+        if(fast === clickedOne) {
+            good.checked = false
+        }
+    }
+}
