12c14cad72f1e98357b8a12531a329ac13c27994
Maria Sargent <75868918+mesgt@users.noreply.github.com>
2021-06-09 21:01:23 -0500

    reset function added.

--- AnimatedCountdown/script.js
+++ AnimatedCountdown/script.js
@@ -5,15 +5,26 @@ const replay = document.querySelector('#replay')
 
 runAnimation()
 
+function resetDOM() {
+    counter.classList.remove('hide')
+    finalMessage.classList.remove('show')
+
+    num.forEach(num => {
+        num.classList.value = ''
+    })
+
+    num[0].classList.add('in') //resets everything to initial state
+}
+
 function runAnimation() {
     nums.forEach((num, idx) => {
         const nextToLast = nums.length - 1
 
         num.addEventListener('animationend', (e) => {
-            if(e.animationName === 'goIn' && idx !== nextToLast) {
+            if (e.animationName === 'goIn' && idx !== nextToLast) {
                 num.classList.remove('in')
                 num.classList.add('out')
-            } else if(e.animationName === 'goOut' && num.nextElementSibling) {
+            } else if (e.animationName === 'goOut' && num.nextElementSibling) {
                 num.nextElementSibling.classList.add('in')
             } else {
                 counter.classList.add('hide')
@@ -22,3 +33,4 @@ function runAnimation() {
         })
     })
 }
+
