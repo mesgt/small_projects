31abb2a64415a464069d985a720b0b358ef0645c
Maria Sargent <75868918+mesgt@users.noreply.github.com>
2021-06-10 22:52:59 -0500

    container with squares added for styling.

--- ImageCarousel/script.js
+++ ImageCarousel/script.js
@@ -3,8 +3,6 @@ const leftBtn = document.getElementById('left')
 const rightBtn = document.getElementById('right')
 const img = document.querySelectorAll('#imgs img')
 
-console.log(img)
-
 let idx = 0
 let interval = setInterval(run, 2000)
 
