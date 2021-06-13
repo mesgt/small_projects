fbcc9a71a7011095f3e325b0d04e72cc0f37e9c9
Maria Sargent <75868918+mesgt@users.noreply.github.com>
2021-06-12 20:01:32 -0500

    user able to delete incorrect entry.

--- VerifyAcctUI/script.js
+++ VerifyAcctUI/script.js
@@ -4,10 +4,14 @@ codes[0].focus()
 
 codes.forEach((code, idx) => {
     code.addEventListener('keydown', (e) => {
-        if(e.key >= 0 && e.key <= 9) {
+        if(e.key >= 0 && e.key <= 9 && idx < codes.length) {
+            codes[idx].value = ''
             setTimeout(() => codes[idx + 1].focus(), 10)
         } else if(e.key === 'Backspace') {
             setTimeout(() => codes[idx - 1].focus(), 10)
+        } else if(idx < codes.length){
+            
+            //prevent further typing in this field
         }
     })
 })
