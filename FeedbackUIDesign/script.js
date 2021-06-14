6fb91d5ba7874143ed140d5e99158819c4383e78
Maria Sargent <75868918+mesgt@users.noreply.github.com>
2021-06-14 13:16:47 -0500

    send btn submission response.

--- FeedbackUIDesign/script.js
+++ FeedbackUIDesign/script.js
@@ -2,13 +2,25 @@ const ratings = document.querySelectorAll('.rating')
 const sendBtn = document.querySelector('#send')
 const panel = document.querySelector('#panel')
 
+let selectedRating = 'Satisfied'
+
 panel.addEventListener('click', (e) => {
     if(e.target.parentNode.classList.contains('rating')) {
         removeActive()
         e.target.parentNode.classList.add('active')
-
-        // console.log(e.target)
+        selectedRating = e.target.nextElementSibling.innerHTML
+        console.log(selectedRating)
     }
+})
+
+sendBtn.addEventListener('click', () => {
+    panel.innerHTML = `
+    <i class="fas fa-heart"></i>
+    <strong>Thank you!</strong>
+    <br>
+    <strong>Feedback: ${selectedRating}</strong>
+    <p>We'll use your feedback to improve our customer support.</p>
+    `
 })
 
 function removeActive() {
