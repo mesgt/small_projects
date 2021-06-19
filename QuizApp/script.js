af92a44bf41a11fce6c9d598ab03cc2c6efeab85
Maria Sargent <75868918+mesgt@users.noreply.github.com>
2021-06-19 14:26:20 -0500

    quizData.

---
+++ QuizApp/script.js
@@ -0,0 +1,37 @@
+
+
+const quizData = [
+    {
+        question: "Which language tuns in a web broser?",
+        a: "Java",
+        b: "C",
+        c: "Python",
+        d: "JavaScript",
+        correct: "d",
+    },
+    {
+        question: "What does CSS stand for?",
+        a: "Central Style Sheets",
+        b: "Cascading Style Sheets",
+        c: "Cascading Simple Sheets",
+        d: "Cars SUVs Sailboats",
+        correct: "b",
+    },
+    {
+        question: "What does HTML stand for?",
+        a: "Hypertext Markup Language",
+        b: "Hypertext Markdown Language",
+        c: "Hyperloop Machine Language",
+        d: "Helicopter Terminal Medicine Language",
+        correct: "a",
+    },
+    {
+        question: "What year was JavaScript launched?",
+        a: "1996",
+        b: "1995",
+        c: "1994",
+        d: "It's still under construction",
+        correct: "b",
+    },
+];
+
