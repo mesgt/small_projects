80f77c3d0b2559d7468cf675e922533c1ebae3a0
Maria Sargent <75868918+mesgt@users.noreply.github.com>
2021-06-19 14:33:20 -0500

    loadQuiz function.

--- QuizApp/script.js
+++ QuizApp/script.js
@@ -5,6 +5,7 @@ const a_text = document.getElementById('a_text')
 const b_text = document.getElementById('b_text')
 const c_text = document.getElementById('c_text')
 const d_text = document.getElementById('d_text')
+const submitBtn = document.getElementById('submit')
 
 const quizData = [
     {
@@ -41,3 +42,17 @@ const quizData = [
     },
 ];
 
+let currentQuiz = 0
+let score = 0
+
+loadQuiz()
+
+function loadQuiz() {
+    const currentQuizData = quizData[currentQuiz]
+
+    questionEl.innerText = currentQuizData.question
+    a_text.innerText = currentQuizData.a
+    b_text.innerText = currentQuizData.b
+    c_text.innerText = currentQuizData.c
+    d_text.innerText = currentQuizData.d
+}
