d3d76e53cc51f7d32404346d9b7d2c55703949dc
Maria Sargent <75868918+mesgt@users.noreply.github.com>
2021-06-21 21:42:08 -0500

    testimonials array.

---
+++ TestimonialBoxSwitcher/script.js
@@ -0,0 +1,33 @@
+const testimonialsContainer = document. querySelector('.testimonials-container')
+const testimonials = document. querySelector('.testimonial')
+const userImage = document. querySelector('.user-image')
+const role = document. querySelector('.role')
+const userName = document. querySelector('.user-name')
+
+
+const testimonials = [
+    {
+        name: 'Leon Green',
+        position: 'CEO',
+        photo: 'https://randomuser.me/api/portraits/men/58.jpg',
+        text: 'Thrive’s website design team far exceeded my expectations on every front, and I have very high expectations. The end product is a beautiful website that embodies who we are as company. Every person I worked with at Thrive was knowledgeable, creative, prompt, and personable.',
+    },
+    {
+        name: 'Chad Montgomery',
+        position: 'Manager',
+        photo: 'https://randomuser.me/api/portraits/men/35.jpg',
+        text: 'Working with the Thrive team has been a most pleasant experience! Their dedication and passion for what they do is exemplified by their unrivaled customer support and attentiveness to the specific needs of our business. We look forward to a long lasting and prosperous relationship with the crew at Thrive.',
+    },
+    {
+        name: 'Cass Roberts',
+        position: 'Marketing Coordinator',
+        photo: 'https://randomuser.me/api/portraits/women/35.jpg',
+        text: 'Thrive is a great company to work with as they are always responsive and willing to do what it takes to get the job done right and in a timely manner. They are very reliable and intelligent. We interviewed several Marketing firms to revamp our website, logo, and other items, and it was clear right away that Thrive was the right choice.',
+    },
+    {
+        name: 'Marci Weirsma',
+        position: 'Minister of Communications',
+        photo: 'https://randomuser.me/api/portraits/women/65.jpg',
+        text: 'We found Thrive after years of searching for the right SEO company and failing. They were a much needed blessing and exceeded our expectations in every way. After years of paying and not receiving what we were paying for, at the cost of our business and financial growth, we cannot say enough about them and highly recommend them. They are honest, straight-forward, they take care of ALL your needs quickly, they are reliable, you can count on them and most of all, they do everything they say they will do, no BS.',
+    },
+]
