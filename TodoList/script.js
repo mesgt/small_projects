d46bc441304822216c76d490da6ee725146a5b1e
Maria Sargent <75868918+mesgt@users.noreply.github.com>
2021-06-21 22:51:03 -0500

    todos stored in local storage.

--- TodoList/script.js
+++ TodoList/script.js
@@ -2,6 +2,12 @@ const form = document.getElementById('form')
 const todosUL = document.getElementById('todos')
 const input = document.getElementById('input')
 
+const todos = JSON.parse(localStorage.getItem('todos'))
+
+if(todos) {
+    todos.forEach(todo => addTodo(todo))
+}
+
 form.addEventListener('submit', (e) => {
     e.preventDefault()
 
@@ -33,5 +39,20 @@ function addTodo(todo) {
         todosUL.appendChild(todoEl)
 
         input.value = ''
+
+        updateLS()
     }
+}
+
+function updateLS() {
+    todosEl = document.querySelectorAll('li')
+    const todos = []
+
+    todosEl.forEach(todoEl => {
+        todos.push({
+            text: todoEl.innerText,
+            completed: todoEl.classList.contains('completed')
+        })
+    })
+    localStorage.setItem('todos', JSON.stringify(todos))
 }
