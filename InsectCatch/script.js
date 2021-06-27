968c5af53c3c5aa2eece22374459979c4a38c1cb
Maria Sargent <75868918+mesgt@users.noreply.github.com>
2021-06-26 21:44:41 -0500

    choose insect btn properties added.

--- InsectCatch/script.js
+++ InsectCatch/script.js
@@ -11,3 +11,24 @@ let score = 0
 let selected_insect = {}
 
 start_btn.addEventListener('click', () => screens[0].classList.add('up'))
+
+choose_insect_btns.forEach(btn => {
+    btn.addEventListener('click', () => {
+        const img = btn.querySelector('img')
+        const src = img.getAttribute('src')
+        const alt = img.getAttribute('alt')
+        selected_insect = { src, alt }
+        screens[1].classList.add('up')
+        setTimeout(createInsect, 1000)
+        startGame()
+    })
+})
+
+function createInsect() {
+    const insect = document.createElement('div')
+    insect.classList.add('insect')
+}
+
+function startGame() {
+
+}
