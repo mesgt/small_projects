ea1ddb4027ba0ac27b61a7a4152ca6ead3c009d7
Maria Sargent <75868918+mesgt@users.noreply.github.com>
2021-06-29 22:48:57 -0500

    background color selected based on random number generation.

---
+++ ColorFlipper/app.js
@@ -0,0 +1,16 @@
+const colors = ["green", "red", "rgba(133, 122, 200)", "#f15025"]
+const btn = document.getElementById('btn')
+const color = document.querySelector('.color')
+
+btn.addEventListener('click', () => {
+    const randomNum = getRandomNum()
+    console.log(randomNum)
+
+    document.body.style.backgroundColor = colors[randomNum]
+    color.textContent = colors[randomNum]
+})
+
+
+function getRandomNum() {
+    return Math.floor(Math.random() * 4)
+}
