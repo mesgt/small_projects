600ef15d813bd4c224c2be1712bb09bdcbd128cb
Maria Sargent <75868918+mesgt@users.noreply.github.com>
2021-07-02 16:04:53 -0500

    close and toggle btn are functional.

---
+++ Sidebar/app.js
@@ -0,0 +1,11 @@
+const closeBtn = document.querySelector('.close-btn')
+const toggleBtn = document.querySelector('.sidebar-toggle')
+const sidebar = document.querySelector('.sidebar')
+
+toggleBtn.addEventListener('click', () => {
+    sidebar.classList.toggle('show-sidebar')
+})
+
+closeBtn.addEventListener('click', () => {
+    sidebar.classList.remove('show-sidebar')
+})
